4fe92218a8e7af5a86833c38b5b01ee02782dd56
Ian Cooper <iancooper@hotmail.com>
2026-04-02 05:08:44 +0100

    luci-app-mwan3: clarify flush_conntrack help text
    Update the help text for the flush_conntrack option to clarify that
    it flushes the entire global conntrack table, and that per-interface
    conntrack entries are already flushed automatically on ifdown since
    commit ("mwan3: selectively flush conntrack entries for failed WAN
    interface on ifdown").
    
    Signed-off-by: Ian Cooper <iancooper@hotmail.com>

--- htdocs/luci-static/resources/view/mwan3/network/interface.js
+++ htdocs/luci-static/resources/view/mwan3/network/interface.js
@@ -284,7 +284,7 @@ return view.extend({
 		o.value('10');
 
 		o = s.option(form.DynamicList, 'flush_conntrack', _('Flush conntrack table'),
-			_('Flush global firewall conntrack table on interface events'));
+			_('Flush the entire global conntrack table on selected events. Per-interface conntrack entries are already flushed automatically on ifdown.'));
 		o.value('ifup', _('ifup (netifd)'));
 		o.value('ifdown', _('ifdown (netifd)'));
 		o.value('connected', _('connected (mwan3)'));
